8930e86d6fde93d509245729ad466caead41859e
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-20 20:26:00 -0300

    a

--- crud-pessoa/insert.php
+++ crud-pessoa/insert.php
@@ -1,4 +1,6 @@
 <?php
+$result = mysqli_query($conexao, "INSERT INTO pessoa(nome, telefone, email) 
+VALUES ('ggggg,,', '319995533374', 'g@gmail')"); 
 
-INSERT INTO pessoa (nome, telefone, email) VALUES ('ggggg,,', '319995533374', 'g@gmail');
+
 ?>
