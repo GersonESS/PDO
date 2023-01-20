670389b35a3940445fd302fd99b6853a75896e4a
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-20 10:09:40 -0300

    a

---
+++ CONEXAO.php
@@ -0,0 +1,8 @@
+<!-- create database crudpdo;
+use crudpdo;
+create table pessoa (
+    id int auto_invrement prymary key,
+    nome varchar(40),
+    telefone varchar(15),
+    email varchar(15)
+); -->
---
+++ crud-pdo.php
@@ -0,0 +1,23 @@
+<?php
+// ------------------------CONEXAO-----------------------------------
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = 'gabibi89'; 
+    $dbname = 'CRUDPDO';
+
+    $pdo = new mysqli($dbHost,$dbUsername,$dbPassword,$dbname);
+    if($pdo->connect_errno){
+        echo "Erro na Conexao";
+    }
+    else
+    {
+        echo "Conexao realizada com sucesso";
+    }
+ // ------------------------insert-----------------------------------
+ // ----PHP PDO 2/10 (INSERT)
+ // ------------------------delete e update-----------------------------------
+ // --PHP PDO 3/10 (DELETE E UPDATE)
+// -----------------------------------------------------------
+
+     
+?> 
