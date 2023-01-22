66d5373cb86401026f3cb96feefc8f1a7d26a2d0
Gerson Santos <62805855+GersonESS@users.noreply.github.com>
2023-01-22 20:06:21 -0300

    a

--- crud-pessoa/classe-pessoa.php
+++ crud-pessoa/classe-pessoa.php
@@ -22,8 +22,7 @@ Class Pessoa
     public function buscarDados()
     {
         $res = array();
-        //$cmd = $this->pdo->query("SELECT * FROM pessoa ORDER BY nome");
-        $cmd = $this->pdo->prepare("SELECT * FROM pessoa ORDER BY nome");
+        $cmd = $this->pdo->query("SELECT * FROM pessoa ORDER BY nome"); 
         $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
         return $res;
     }
